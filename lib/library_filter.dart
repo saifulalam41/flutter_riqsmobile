ffc50e02c25dbad8effee1132c51478546a29803
saifulalam41 <saiful.alam@selisegroup.com>
2024-05-05 00:11:40 +0600

    filter design

--- lib/library_filter.dart
+++ lib/library_filter.dart
@@ -10,10 +10,20 @@ class LibraryFilter extends StatefulWidget {
 }
 
 class _LibraryFilter extends State<LibraryFilter> {
+ late DateTime _dateTime;
+ getDate() async {
+   DateTime: var date= await showDatePicker(
+       context: context,
+       initialDate: DateTime(DateTime.now().year),
+       firstDate: DateTime(DateTime.now().year-20),
+       lastDate: DateTime(DateTime.now().year+2));
+
+ }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:const EdgeInsets.all(20) ,
+      padding: const EdgeInsets.all(20),
       child: Container(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -21,7 +31,8 @@ class _LibraryFilter extends State<LibraryFilter> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text('Employee.pdf',
+                Text(
+                  'Employee.pdf',
                   style: TextStyle(
                     color: Colors.black,
                     fontSize: 20,
@@ -38,64 +49,71 @@ class _LibraryFilter extends State<LibraryFilter> {
             ),
             Container(
               margin: EdgeInsets.only(top: 16.0),
-              padding:const EdgeInsets.all(14) ,
+              padding: const EdgeInsets.all(14),
               decoration: BoxDecoration(
                 border: Border.all(
                   color: const Color.fromARGB(255, 200, 200, 200),
                   width: 1.0,
                 ),
-
-
                 borderRadius: BorderRadius.circular(5.0),
               ),
-
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text('Select a date', style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,letterSpacing: 0.8)),
-                  Icon(Icons.calendar_month, color: const Color.fromARGB(255, 20, 94, 74)),
+                  Text('Select a date',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          letterSpacing: 0.8)),
+                  Icon(Icons.calendar_month,
+                      color: const Color.fromARGB(255, 20, 94, 74)),
                 ],
               ),
             ),
             Container(
               margin: EdgeInsets.only(top: 20.0),
-              padding:const EdgeInsets.all(14) ,
+              padding: const EdgeInsets.all(14),
               decoration: BoxDecoration(
                 border: Border.all(
                   color: const Color.fromARGB(255, 200, 200, 200),
                   width: 1.0,
                 ),
-
-
                 borderRadius: BorderRadius.circular(5.0),
               ),
-
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text('Select a date', style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,letterSpacing: 0.8)),
-                  Icon(Icons.calendar_month, color: const Color.fromARGB(255, 20, 94, 74)),
+                  Text('Select a date',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          letterSpacing: 0.8)),
+                  Icon(Icons.calendar_month,
+                      color: const Color.fromARGB(255, 20, 94, 74)),
                 ],
               ),
             ),
             Container(
               margin: EdgeInsets.only(top: 20.0),
-              padding:const EdgeInsets.all(14) ,
+              padding: const EdgeInsets.all(14),
               decoration: BoxDecoration(
                 border: Border.all(
                   color: const Color.fromARGB(255, 200, 200, 200),
                   width: 1.0,
                 ),
-
-
                 borderRadius: BorderRadius.circular(5.0),
               ),
-
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text('Assigned To', style: TextStyle(fontWeight: FontWeight.w500,color: const Color.fromARGB(255, 0,0,0),fontSize: 16,letterSpacing: 0.8)),
-                  Icon(Icons.expand_more, color: const Color.fromARGB(255, 20, 94, 74)),
+                  Text('Assigned To',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 16,
+                          letterSpacing: 0.8)),
+                  Icon(Icons.expand_more,
+                      color: const Color.fromARGB(255, 20, 94, 74)),
                 ],
               ),
             ),
@@ -104,15 +122,16 @@ class _LibraryFilter extends State<LibraryFilter> {
               child: TextField(
                 decoration: InputDecoration(
                   hintText: 'Version',
-                  contentPadding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 14),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 14.0, horizontal: 14),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(5.0),
                     borderSide: BorderSide(
-                      color: const Color.fromARGB(255, 200, 200, 200), // Border color
-                      width: 1.0,        // Border width
+                      color: const Color.fromARGB(
+                          255, 200, 200, 200), // Border color
+                      width: 1.0, // Border width
                     ),
                   ),
-
                 ),
                 // decoration: InputDecoration(
                 //   hintText: 'Search...',
@@ -134,7 +153,8 @@ class _LibraryFilter extends State<LibraryFilter> {
                       // primary: Colors.red, // Background color
                       // onPrimary: Colors.white, // Text color
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0), // Set the border radius
+                        borderRadius:
+                            BorderRadius.circular(5.0), // Set the border radius
                       ),
                     ),
                     child: Text('CANCEL'),
@@ -147,26 +167,28 @@ class _LibraryFilter extends State<LibraryFilter> {
                         Navigator.of(context).pop();
                         // Add save button logic here
                       },
-
                       style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 20, 94, 74),
+                        backgroundColor: Color.fromARGB(255, 20, 94, 74),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0), // Set the border radius
+                          borderRadius: BorderRadius.circular(
+                              10.0), // Set the border radius
                         ),
                         // primary: Colors.green, // Background color
                         // onPrimary: Colors.white, // Text color
                       ),
-                      child: Text('APPLY', style: TextStyle(fontWeight: FontWeight.w500,color: Colors.white,fontSize: 16,letterSpacing: 0.8)),
+                      child: Text('APPLY',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                              fontSize: 16,
+                              letterSpacing: 0.8)),
                     ),
                   ),
                 ],
               ),
             ),
-
           ],
         ),
-
-
       ),
     );
   }
